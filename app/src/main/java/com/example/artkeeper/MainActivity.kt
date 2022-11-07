04cb6744cc8b241351d14453b95e09447f56eb3b
francescofranco <frafranco16@gmail.com>
2022-11-07 16:17:49 +0100

    aggiunta controllo se l'utente ha effettuato l'accesso

--- app/src/main/java/com/example/artkeeper/MainActivity.kt
+++ app/src/main/java/com/example/artkeeper/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.artkeeper
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
@@ -8,7 +9,9 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.firebase.ui.auth.AuthUI
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
@@ -17,18 +20,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        navController = navHostFragment.navController
+        //AuthUI.getInstance().signOut(this)
+        if(FirebaseAuth.getInstance().currentUser == null) {
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        }
+        else {
+            val navHostFragment =
+                supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+            navController = navHostFragment.navController
 
 
-        findViewById<BottomNavigationView>(R.id.bottom_nav).setupWithNavController(navController)
-        appBarConfiguration = AppBarConfiguration(
-            setOf(R.id.mainFragment, R.id.newPost, R.id.profileFragment)
-        )
-        setupActionBarWithNavController(
-            navController, appBarConfiguration
-        )
+            findViewById<BottomNavigationView>(R.id.bottom_nav).setupWithNavController(navController)
+            appBarConfiguration = AppBarConfiguration(
+                setOf(R.id.mainFragment, R.id.newPost, R.id.profileFragment)
+            )
+            setupActionBarWithNavController(
+                navController, appBarConfiguration
+            )
+        }
     }
 
 
