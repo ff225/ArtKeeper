cab81d6f12ea4629c06d7255fe576a30b2eb7ad0
francescofranco <frafranco16@gmail.com>
2022-11-10 18:42:48 +0100

    first commit UserDao

---
+++ app/src/main/java/com/example/artkeeper/data/UserDao.kt
@@ -0,0 +1,21 @@
+package com.example.artkeeper.data
+
+import androidx.room.*
+import com.example.artkeeper.data.model.User
+
+@Dao
+interface UserDao {
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(user: User)
+
+    @Update
+    suspend fun update(user: User)
+
+    /*
+    @Delete
+    suspend fun delete(uid: String)
+*/
+    @Query("SELECT * FROM users WHERE uid=:uid")
+    fun getUser(uid: String): User
+}
