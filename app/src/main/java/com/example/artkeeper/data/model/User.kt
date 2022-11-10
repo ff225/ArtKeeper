788393d2699aec0668c607136beb69d60f30c8fa
francescofranco <frafranco16@gmail.com>
2022-11-10 18:51:00 +0100

    creata entità user

--- app/src/main/java/com/example/artkeeper/data/model/User.kt
+++ app/src/main/java/com/example/artkeeper/data/model/User.kt
@@ -1,11 +1,16 @@
 package com.example.artkeeper.data.model
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "users")
 data class User(
-    val id: Int,
-    val firstName: String,
-    val lastName: String,
-    val nickName: String,
-    val nChild: Int,
-    val nameChild: List<String>,
-    val nPosts: Int
+    @PrimaryKey val uid: String,
+    @ColumnInfo(name = "first_name") val firstName: String,
+    @ColumnInfo(name = "last_name")val lastName: String,
+    @ColumnInfo(name = "nickname")val nickName: String,
+    @ColumnInfo(name = "num_child")val nChild: Int,
+    //TODO risolvere problema legato all'array di stringhe name_child
+    @ColumnInfo(name = "name_child") val nameChild: String
 )
