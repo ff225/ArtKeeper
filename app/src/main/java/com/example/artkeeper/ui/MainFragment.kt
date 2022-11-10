085faa4fbf6159ed32e9cd65a9b4a822d8b14767
francescofranco <frafranco16@gmail.com>
2022-11-10 18:50:19 +0100

    aggiunto controllo utente connesso, navigazione verso login

--- app/src/main/java/com/example/artkeeper/ui/MainFragment.kt
+++ app/src/main/java/com/example/artkeeper/ui/MainFragment.kt
@@ -5,29 +5,45 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isGone
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.coroutineScope
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.RecyclerView
+import com.example.artkeeper.R
 import com.example.artkeeper.adapter.PostAdapter
 import com.example.artkeeper.databinding.FragmentMainBinding
 import com.example.artkeeper.utils.ArtKeeper
 import com.example.artkeeper.viewmodel.PostViewModel
 import com.example.artkeeper.viewmodel.PostViewModelFactory
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class MainFragment : Fragment() {
+    companion object {
+        const val TAG = "MainFragment"
+    }
 
     private var _binding: FragmentMainBinding? = null
     private val binding: FragmentMainBinding
         get() = _binding!!
     private lateinit var recyclerView: RecyclerView
     private val viewModel: PostViewModel by activityViewModels { PostViewModelFactory((activity?.application as ArtKeeper).database.postDao()) }
+
+    
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        Log.d(TAG, "onCreateView")
+        Log.d(TAG, "${FirebaseAuth.getInstance().currentUser}")
+        if (FirebaseAuth.getInstance().currentUser == null) {
+            findNavController().navigate(R.id.login)
+        }
+        activity?.findViewById<BottomNavigationView>(R.id.bottom_nav)!!.isGone = false
         _binding = FragmentMainBinding.inflate(inflater, container, false)
         // Inflate the layout for this fragment
         return binding.root
@@ -47,7 +63,7 @@ class MainFragment : Fragment() {
     }
 
     override fun onDestroy() {
-        Log.d("MainFragment", "destroyed")
+        Log.d(TAG, "onDestroy")
         _binding = null
         super.onDestroy()
     }
