8be1578b408466f64c37a308e9640092a8958e91
francescofranco <frafranco16@gmail.com>
2022-11-14 15:37:58 +0100

    initial commit

---
+++ app/src/main/java/com/example/artkeeper/data/datasource/PostLocalDataSource.kt
@@ -0,0 +1,28 @@
+package com.example.artkeeper.data.datasource
+
+import com.example.artkeeper.data.PostDao
+import com.example.artkeeper.data.model.Post
+import com.example.artkeeper.utils.Resource
+import kotlinx.coroutines.flow.Flow
+
+
+class PostLocalDataSource(private val postDao: PostDao) {
+    suspend fun insert(post: Post) {
+        postDao.insert(post)
+    }
+
+    suspend fun update(post: Post) {
+        postDao.update(post)
+    }
+
+    suspend fun delete(post: Post) {
+        postDao.delete(post)
+    }
+
+    fun getNumPost(uid: String): Flow<Int> = postDao.getNumPost(uid)
+
+    fun getAllUserPost(uid: String): Flow<List<Post>> = postDao.getUserPosts(uid)
+
+    fun getAllPost(): Flow<List<Post>> = postDao.getPosts()
+
+}
