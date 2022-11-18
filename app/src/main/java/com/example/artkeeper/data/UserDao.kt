6fe5381e84faa92ea2a14113a993bb765a36d410
francescofranco <frafranco16@gmail.com>
2022-11-18 16:44:46 +0100

    aggiunto query checkUser

--- app/src/main/java/com/example/artkeeper/data/UserDao.kt
+++ app/src/main/java/com/example/artkeeper/data/UserDao.kt
@@ -2,6 +2,7 @@ package com.example.artkeeper.data
 
 import androidx.room.*
 import com.example.artkeeper.data.model.User
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface UserDao {
@@ -16,5 +17,8 @@ interface UserDao {
     suspend fun delete(user: User)
 
     @Query("SELECT * FROM users WHERE uid=:uid")
-    suspend fun getUser(uid: String): User?
+    fun getUser(uid: String): Flow<User>
+
+    @Query("SELECT count(*) FROM users WHERE uid= :uid")
+    fun checkUser(uid: String): Boolean
 }
