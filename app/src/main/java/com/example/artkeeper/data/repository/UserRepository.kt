9fcbbb3a5fbc6928be5c001d107065fc1710209d
francescofranco <frafranco16@gmail.com>
2022-12-20 11:52:32 +0100

    aggiunto checkNickname

--- app/src/main/java/com/example/artkeeper/data/repository/UserRepository.kt
+++ app/src/main/java/com/example/artkeeper/data/repository/UserRepository.kt
@@ -4,9 +4,15 @@ import com.example.artkeeper.data.datasource.UserLocalDataSource
 import com.example.artkeeper.data.model.User
 import kotlinx.coroutines.flow.Flow
 
-class UserRepository(private val userLocalDataSource: UserLocalDataSource) {
+class UserRepository(
+    private val userLocalDataSource: UserLocalDataSource
+) {
 
     suspend fun insertUser(user: User) = userLocalDataSource.insert(user)
+
+    suspend fun checkNickname(nickName: String): Boolean =
+        userLocalDataSource.checkNickname(nickName)
+
     suspend fun updateUser(user: User) = userLocalDataSource.update(user)
     suspend fun deleteUser(user: User) = userLocalDataSource.delete(user)
     suspend fun addChild(uid: String, nChild: Int, nameChild: List<String>) =
