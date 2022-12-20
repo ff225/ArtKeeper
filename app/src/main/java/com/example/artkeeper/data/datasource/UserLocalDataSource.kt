28fe7ecdd1f2e3a6add07cc8cefa3421245af818
francescofranco <frafranco16@gmail.com>
2022-12-20 11:51:59 +0100

    aggiunto dispatcher, modificata chiamata coroutine

--- app/src/main/java/com/example/artkeeper/data/datasource/UserLocalDataSource.kt
+++ app/src/main/java/com/example/artkeeper/data/datasource/UserLocalDataSource.kt
@@ -2,15 +2,33 @@ package com.example.artkeeper.data.datasource
 
 import com.example.artkeeper.data.UserDao
 import com.example.artkeeper.data.model.User
+import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.Flow
 
-class UserLocalDataSource(private val userDao: UserDao) {
+class UserLocalDataSource(
+    private val userDao: UserDao,
+    private val dispatcher: CoroutineDispatcher = Dispatchers.IO
+) {
 
-    suspend fun insert(user: User) = userDao.insert(user)
-    suspend fun update(user: User) = userDao.update(user)
-    suspend fun delete(user: User) = userDao.delete(user)
+    suspend fun insert(user: User) {
+        withContext(dispatcher) { async { userDao.insert(user) } }.await()
+    }
+
+    suspend fun checkNickname(nickname: String): Boolean {
+        return withContext(dispatcher) { async { userDao.checkNickname(nickname) } }.await()
+    }
+
+
+    suspend fun update(user: User) =
+        withContext(dispatcher) { async { userDao.update(user) } }.await()
+
+    suspend fun delete(user: User) =
+        withContext(CoroutineScope(dispatcher).coroutineContext) {
+            userDao.delete(user)
+        }
+
     suspend fun addChild(uid: String, nChild: Int, nameChild: List<String>) =
-        userDao.addChild(uid, nChild, nameChild)
+        withContext(dispatcher) { userDao.addChild(uid, nChild, nameChild) }
 
     fun getUser(uid: String): Flow<User> = userDao.getUser(uid)
     fun checkUser(uid: String): Boolean = userDao.checkUser(uid)
