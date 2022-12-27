7054acd52a0f6eab01728bc7f6053f663f4e3784
francescofranco <frafranco16@gmail.com>
2022-12-27 09:52:48 +0100

    change signature, added UserRemoteDataSource

--- app/src/main/java/com/example/artkeeper/data/repository/UserRepository.kt
+++ app/src/main/java/com/example/artkeeper/data/repository/UserRepository.kt
@@ -1,26 +1,39 @@
 package com.example.artkeeper.data.repository
 
 import com.example.artkeeper.data.datasource.UserLocalDataSource
+import com.example.artkeeper.data.datasource.UserRemoteDataSource
 import com.example.artkeeper.data.model.User
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.distinctUntilChanged
 
 class UserRepository(
-    private val userLocalDataSource: UserLocalDataSource
+    private val userLocalDataSource: UserLocalDataSource,
+    private val userRemoteDataSource: UserRemoteDataSource
 ) {
 
-    suspend fun insertUser(user: User) = userLocalDataSource.insert(user)
+    suspend fun insertUserLocal(user: User) = userLocalDataSource.insert(user)
 
-    suspend fun checkNickname(nickName: String): Boolean =
+    suspend fun checkNicknameLocal(nickName: String): Boolean =
         userLocalDataSource.checkNickname(nickName)
 
-    suspend fun updateUser(user: User) = userLocalDataSource.update(user)
-    
-    suspend fun deleteUser(user: User) = userLocalDataSource.delete(user)
+    suspend fun updateUserLocal(user: User) = userLocalDataSource.update(user)
 
-    suspend fun addChild(uid: String, nChild: Int, nameChild: List<String>) =
+    suspend fun deleteUserLocal(user: User) = userLocalDataSource.delete(user)
+
+    suspend fun addChildLocal(uid: String, nChild: Int, nameChild: List<String>) =
         userLocalDataSource.addChild(uid, nChild, nameChild)
 
     suspend fun checkUser(uid: String): Boolean = userLocalDataSource.checkUser(uid)
 
-    fun getUser(uid: String): Flow<User> = userLocalDataSource.getUser(uid)
+    fun getUserLocal(uid: String): Flow<User> = userLocalDataSource.getUser(uid).distinctUntilChanged()
+
+    suspend fun insertUserRemote(user: User) = userRemoteDataSource.insertUser(user)
+
+    suspend fun checkNicknameRemote(nickName: String): Boolean =
+        userRemoteDataSource.checkNickname(nickName)
+
+    suspend fun deleteUserRemote() = userRemoteDataSource.deleteUser()
+
+    suspend fun addChildRemote(nChild: Int, nameChild: List<String>) =
+        userRemoteDataSource.addSon(nChild, nameChild)
 }
