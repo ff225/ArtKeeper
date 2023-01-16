58be7b130fc453bcdec75ceb8ba1f1281d4cdc02
francescofranco <frafranco16@gmail.com>
2023-01-16 10:45:03 +0100

    add Constants

---
+++ app/src/main/java/com/example/artkeeper/utils/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.artkeeper.utils
+
+import com.google.firebase.auth.FirebaseAuth
+
+object Constants {
+    val firebaseAuth = FirebaseAuth.getInstance()
+    const val TAG_LOGIN_FRAGMENT = "LoginFragment"
+}
