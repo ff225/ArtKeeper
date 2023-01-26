44bd870576bbe953f45d075b6e8f1697c88baf47
Francesco Franco <frafranco16@gmail.com>
2023-01-26 15:23:04 +0100

    Utilizzo di postRemote, aggiunto nickname dinamico

--- app/src/main/java/com/example/artkeeper/adapter/PostAdapter.kt
+++ app/src/main/java/com/example/artkeeper/adapter/PostAdapter.kt
@@ -3,82 +3,90 @@ package com.example.artkeeper.adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.net.toUri
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DecodeFormat
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.example.artkeeper.R
-import com.example.artkeeper.data.model.Post
+import com.example.artkeeper.data.model.PostRemote
 import com.example.artkeeper.databinding.PostItemBinding
 import java.text.DateFormat
 import java.util.*
 
-class PostAdapter(private var optionsMenuClickListener: OptionsMenuClickListener) :
-    ListAdapter<Post, PostAdapter.PostAdatperViewHolder>(DiffCallback) {
+class PostAdapter :
+    ListAdapter<PostRemote, PostAdapter.PostItemViewHolder>(PostRemoteDiffCallback()) {
 
-    interface OptionsMenuClickListener {
-        fun onOptionsMenuClicked(post: Post, position: Int)
+    var nickName: String = ""
+        set(value) {
+            field = value
+            notifyDataSetChanged()
+        }
+
+    class PostItemViewHolder(private val binding: PostItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(post: PostRemote, nickName: String) {
+
+            binding.nickNameItem.text = nickName
+            binding.childNameItem.apply {
+                if (post.sketchedBy != null)
+                    text = post.sketchedBy
+                else
+                    visibility = View.GONE
+            }
+            binding.childNameItem.apply {
+                if (post.sketchedBy.isNullOrEmpty() || post.sketchedBy.equals("null"))
+                    visibility = View.GONE
+                else
+                    text = resources.getString(R.string.sketched_by, post.sketchedBy)
+            }
+            binding.descriptionItem.apply {
+                if (post.description.isNullOrEmpty())
+                    visibility = View.GONE
+                else
+                    text = post.description
+            }
+            binding.timestampPostItem.text =
+                DateFormat.getDateInstance(DateFormat.SHORT)
+                    .format(Date(post.postTimestamp.toLong()))
+
+            Glide.with(binding.photoItem.context)
+                .load(post.imagePath.toUri())
+                .format(DecodeFormat.PREFER_RGB_565)
+                .diskCacheStrategy(DiskCacheStrategy.RESOURCE)
+                .error(R.drawable.ic_baseline_settings_24)
+                .into(binding.photoItem)
+
+        }
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostAdatperViewHolder {
-        val viewHolder = PostAdatperViewHolder(
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostItemViewHolder {
+        return PostItemViewHolder(
             PostItemBinding.inflate(
                 LayoutInflater.from(parent.context),
                 parent,
                 false
             )
         )
-        return viewHolder
     }
 
-    override fun onBindViewHolder(holder: PostAdatperViewHolder, position: Int) {
-        holder.bind(getItem(position), optionsMenuClickListener)
+    override fun onBindViewHolder(holder: PostItemViewHolder, position: Int) {
+
+        holder.bind(getItem(position), nickName)
+
     }
 
-    class PostAdatperViewHolder(private var binding: PostItemBinding) :
-        RecyclerView.ViewHolder(binding.root) {
-        fun bind(post: Post, optionsMenuClickListener: OptionsMenuClickListener) {
-            val date = Date(post.postTimestamp)
-            // modificato da bitmap a URI
-            Glide.with(binding.photoItem.context)
-                .load(
-                    post.imagePath.path
-                )
-                .error(R.drawable.ic_baseline_settings_24)
-                .into(binding.photoItem)
-            //binding.photoItem.setImageURI()
-            binding.nickNameItem.text = post.nickName
-            binding.likeItem.text = post.nLike.toString()
-            binding.childNameItem.apply {
-                if (post.sketchedBy == null)
-                    visibility = View.GONE
-                else
-                    text = resources.getString(R.string.sketched_by, post.sketchedBy)
-            }
-            binding.descriptionItem.apply {
-                if (post.description == null)
-                    visibility = View.GONE
-                else
-                    text = post.description
-            }
-            binding.timestampPostItem.text =
-                DateFormat.getDateInstance(DateFormat.SHORT).format(date)
 
-            binding.textViewOptions.setOnClickListener {
-                optionsMenuClickListener.onOptionsMenuClicked(post, adapterPosition)
-            }
+    class PostRemoteDiffCallback : DiffUtil.ItemCallback<PostRemote>() {
+        override fun areItemsTheSame(oldItem: PostRemote, newItem: PostRemote): Boolean {
+            return oldItem.postTimestamp == newItem.postTimestamp
         }
-    }
 
-    companion object {
-        private val DiffCallback = object : DiffUtil.ItemCallback<Post>() {
-            override fun areItemsTheSame(oldItem: Post, newItem: Post): Boolean {
-                return oldItem.id == newItem.id
-            }
-
-            override fun areContentsTheSame(oldItem: Post, newItem: Post): Boolean {
-                return oldItem == newItem
-            }
+        override fun areContentsTheSame(oldItem: PostRemote, newItem: PostRemote): Boolean {
+            return oldItem == newItem
         }
+
     }
 }
