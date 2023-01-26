1aa0b3c906484a55573f8cdb143ed7e96fecbca9
Francesco Franco <frafranco16@gmail.com>
2023-01-26 15:40:55 +0100

    aggiunta ref a firebase storage

--- app/src/main/java/com/example/artkeeper/utils/Constants.kt
+++ app/src/main/java/com/example/artkeeper/utils/Constants.kt
@@ -1,8 +1,12 @@
 package com.example.artkeeper.utils
 
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 
 object Constants {
     val regex = Regex("^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*\$")
     val firebaseAuth = FirebaseAuth.getInstance()
+    val databaseRef =
+        FirebaseDatabase.getInstance("https://artkeeper-01-default-rtdb.europe-west1.firebasedatabase.app/")
+
 }
