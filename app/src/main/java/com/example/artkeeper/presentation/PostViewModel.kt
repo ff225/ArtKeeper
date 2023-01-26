81f74c0e0406987247137689c745d3df75863157
Francesco Franco <frafranco16@gmail.com>
2023-01-26 15:37:38 +0100

    minor fix + aggiunto worker SavePostRemote

--- app/src/main/java/com/example/artkeeper/presentation/PostViewModel.kt
+++ app/src/main/java/com/example/artkeeper/presentation/PostViewModel.kt
@@ -2,19 +2,26 @@ package com.example.artkeeper.presentation
 
 import android.net.Uri
 import androidx.lifecycle.*
+import androidx.work.*
 import com.example.artkeeper.data.model.Post
+import com.example.artkeeper.data.model.PostRemote
 import com.example.artkeeper.data.model.User
 import com.example.artkeeper.data.repository.PostRepository
 import com.example.artkeeper.data.repository.UserRepository
-import com.google.firebase.auth.FirebaseAuth
-import kotlinx.coroutines.Dispatchers
+import com.example.artkeeper.utils.Constants.firebaseAuth
+import com.example.artkeeper.workers.SavePostRemote
 import kotlinx.coroutines.launch
 import java.util.*
+import java.util.concurrent.TimeUnit
 
-class PostViewModel(private val repo: PostRepository, userRepo: UserRepository) :
+class PostViewModel(
+    private val repo: PostRepository,
+    private val userRepo: UserRepository,
+    private val workManager: WorkManager
+) :
     ViewModel() {
 
-    private val uid: String = FirebaseAuth.getInstance().currentUser!!.uid
+    private val uid: String = firebaseAuth.uid.toString()
     val user: LiveData<User> = userRepo.getUserLocal(uid).asLiveData()
     private var _imageUri: MutableLiveData<Uri?> = MutableLiveData(null)
     val imageUri: LiveData<Uri?>
@@ -23,6 +30,7 @@ class PostViewModel(private val repo: PostRepository, userRepo: UserRepository) 
     private var _description: MutableLiveData<String?> = MutableLiveData(null)
     val description: LiveData<String?>
         get() = _description
+    private var _timestamp: Long = 0L
 
     fun setImageUri(imageUri: Uri) {
         _imageUri.value = imageUri
@@ -40,6 +48,7 @@ class PostViewModel(private val repo: PostRepository, userRepo: UserRepository) 
         _imageUri.value = null
         _description.value = null
         _childName.value = null
+        _timestamp = 0L
     }
 
     fun checkPost(): Boolean {
@@ -50,7 +59,6 @@ class PostViewModel(private val repo: PostRepository, userRepo: UserRepository) 
     }
 
     private fun createPost(): Post {
-
         return Post(
             0,
             uid,
@@ -60,12 +68,53 @@ class PostViewModel(private val repo: PostRepository, userRepo: UserRepository) 
             _childName.value,
             _description.value,
             true,
-            getTimestamp()
+            _timestamp
         )
     }
 
+    private fun createPostRemote(): PostRemote {
+        _timestamp = getTimestamp()
+        return PostRemote(
+            _imageUri.value.toString(),
+            _childName.value,
+            _description.value,
+            _timestamp.toString()
+        )
+    }
+
+    private fun savePostWork() {
+        val constraint = Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.CONNECTED)
+            .build()
+        val postRemote = createPostRemote()
+        val savePostRequest =
+            OneTimeWorkRequestBuilder<SavePostRemote>().setInputData(
+                workDataOf(
+                    "uid" to uid,
+                    "imagePath" to postRemote.imagePath,
+                    "childName" to postRemote.sketchedBy,
+                    "description" to postRemote.description,
+                    "timestamp" to postRemote.postTimestamp
+                )
+            )
+                .setConstraints(constraint).setBackoffCriteria(
+                    BackoffPolicy.LINEAR,
+                    OneTimeWorkRequest.MIN_BACKOFF_MILLIS,
+                    TimeUnit.MILLISECONDS
+                )
+                .build()
+
+        workManager.beginUniqueWork(
+            "savePostRequest",
+            ExistingWorkPolicy.APPEND_OR_REPLACE,
+            savePostRequest
+        )
+            .enqueue()
+    }
+
     fun insert() {
-        viewModelScope.launch(Dispatchers.IO) {
+        savePostWork()
+        viewModelScope.launch {
             repo.insert(createPost())
         }
     }
@@ -80,9 +129,10 @@ class PostViewModel(private val repo: PostRepository, userRepo: UserRepository) 
 
 class PostViewModelFactory(
     private val postRepo: PostRepository,
-    private val userRepo: UserRepository
+    private val userRepo: UserRepository,
+    private val workManager: WorkManager
 ) : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        return PostViewModel(postRepo, userRepo) as T
+        return PostViewModel(postRepo, userRepo, workManager) as T
     }
 }
