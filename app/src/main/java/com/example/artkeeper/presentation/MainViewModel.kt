d8e749655d307553c11f86a4f300c89c7aa754a6
Francesco Franco <frafranco16@gmail.com>
2023-02-01 16:20:55 +0100

    gestione ricerca utenti

--- app/src/main/java/com/example/artkeeper/presentation/MainViewModel.kt
+++ app/src/main/java/com/example/artkeeper/presentation/MainViewModel.kt
@@ -1,19 +1,63 @@
 package com.example.artkeeper.presentation
 
-import androidx.lifecycle.LiveData
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.asLiveData
-import com.example.artkeeper.data.model.Post
-import com.example.artkeeper.data.repository.PostRepository
+import androidx.lifecycle.viewModelScope
+import androidx.work.*
+import com.example.artkeeper.data.repository.UserRepository
+import com.example.artkeeper.workers.UpdateNickname
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.launch
+import java.util.concurrent.TimeUnit
 
-class MainViewModel(private val postRepo: PostRepository) : ViewModel() {
+class MainViewModel(userRepository: UserRepository, private val workManager: WorkManager) :
+    ViewModel() {
 
-    val allPost: LiveData<List<Post>> = postRepo.getAllPost().asLiveData()
+    private val TAG = javaClass.simpleName
+
+    init {
+        viewModelScope.launch {
+            userRepository.insertNicknames()
+        }
+        //updateNickname()
+    }
+
+    val searchQuery = MutableStateFlow("")
+    private val nicknameFlow = searchQuery.flatMapLatest { query ->
+        userRepository.getNicknames(query)
+
+    }
+
+    val nickname = nicknameFlow.asLiveData()
+
+    fun updateNickname() {
+        val constraint = Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.CONNECTED)
+            .build()
+
+        val updateNicknameRequest = PeriodicWorkRequestBuilder<UpdateNickname>(1, TimeUnit.HOURS)
+            .setConstraints(constraint)
+            .build()
+
+        workManager.enqueueUniquePeriodicWork(
+            "UpdateNicknameRequest",
+            ExistingPeriodicWorkPolicy.KEEP,
+            updateNicknameRequest
+        )
+
+        Log.d(TAG, "worker started")
+    }
 }
 
-class MainViewModelFactory(private val postRepo: PostRepository) : ViewModelProvider.Factory {
+class MainViewModelFactory(
+    private val userRepository: UserRepository,
+    private val workManager: WorkManager
+) :
+    ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        return MainViewModel(postRepo) as T
+        return MainViewModel(userRepository, workManager) as T
     }
 }
