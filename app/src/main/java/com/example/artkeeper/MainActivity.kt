3ae8a30cf9bbcc8e74587b10081cf3ded62a0a63
Francesco Franco <frafranco16@gmail.com>
2023-02-13 11:40:57 +0100

    aggiunto richiesta invio notifiche + aggiunto start del servizio

--- app/src/main/java/com/example/artkeeper/MainActivity.kt
+++ app/src/main/java/com/example/artkeeper/MainActivity.kt
@@ -1,13 +1,22 @@
 package com.example.artkeeper
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.artkeeper.utils.Constants
+import com.example.artkeeper.utils.NotificationFollowingRequest
 import com.google.android.material.navigation.NavigationBarView
 
 class MainActivity : AppCompatActivity() {
@@ -22,20 +31,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val serviceIntent = Intent(this, NotificationFollowingRequest::class.java)
+        if (Constants.firebaseAuth.currentUser != null)
+            startService(serviceIntent)
 
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-
         navController = navHostFragment.navController
-        
+
         findViewById<NavigationBarView>(R.id.bottom_nav).apply {
             navController.let {
                 NavigationUI.setupWithNavController(this, navController)
             }
-            setOnItemSelectedListener { item ->
-                NavigationUI.onNavDestinationSelected(item, navController)
-                true
-            }
+            
             setOnItemReselectedListener {
 
             }
@@ -58,6 +66,38 @@ class MainActivity : AppCompatActivity() {
 
     override fun onSupportNavigateUp(): Boolean {
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted: Boolean ->
+        if (isGranted) {
+
+            askNotificationPermission()
+
+        } else {
+            Toast.makeText(this, "b", Toast.LENGTH_LONG).show()
+            askNotificationPermission()
+        }
+    }
+
+    private fun askNotificationPermission() {
+        // This is only necessary for API level >= 33 (TIRAMISU)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) ==
+                PackageManager.PERMISSION_GRANTED
+            ) {
+                // FCM SDK (and your app) can post notifications.
+            } else if (shouldShowRequestPermissionRationale(Manifest.permission.POST_NOTIFICATIONS)) {
+                // TODO: display an educational UI explaining to the user the features that will be enabled
+                //       by them granting the POST_NOTIFICATION permission. This UI should provide the user
+                //       "OK" and "No thanks" buttons. If the user selects "OK," directly request the permission.
+                //       If the user selects "No thanks," allow the user to continue without notifications.
+            } else {
+                // Directly ask for the permission
+                requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+            }
+        }
     }
 
 }
